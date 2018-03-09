bacb60e8ad7ed3b7c8dc3647420c3ca9247936d1
suyashSax <suyash2099@gmail.com>
2018-03-08 21:55:32 -0800

    Condition stuff

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const utils = require('./utils');
 
 const keys = require('./keys/keys');      // key for mLab
 
-// game data 
-require('./models/db');                   
+// game data
+require('./models/db');
 const gameSchema = mongoose.model('gameSchema');
 
 // condition data
-require('./models/conditions');                   
+require('./models/conditions');
 const conditionSchema = mongoose.model('conditionSchema');
 
 // array of game IDs
-var gameArr = ["499530", "499532", "508496", "508495"];
+var gameArr = ["508493"];
 
 // connect to mLab
 mongoose.connect(keys.mongoURI);
@@ -40,13 +40,14 @@ app.get('/message', (req, res) => {
 });
 
 app.post('/condition', (req,res) => {
-
     var condition = new conditionSchema({
+        type: req.body.type,
+        satisfied: false,
         game_id: req.body.game_id,
-        team: req.body.team,
+        team: req.body.team ? req.body.team : null,
+        time: req.body.time ? req.body.time : null,
         goals: req.body.goals
     })
-
     condition.save();
 })
 
@@ -54,14 +55,62 @@ app.listen( 3000, () => {
   console.log("listening on 3000");
 });
 
+setInterval(function testCondition(){
+    conditionSchema.find({satisfied: false}, (err, conditions) => {
+        console.log("Conditions: ", conditions.length);
+        conditions.forEach((condition) => {
+            var gameId = condition.game_id
 
-for(var i = 0; i < gameArr.length; i++){
+            if (condition.type === 1){
+                var goalDiff = condition.goals
+                var timeCondition = condition.time
+                funcs.getLive(gameId, (scores, time) => {
+                    if (time != 0 && time >= timeCondition) {
+                        if (scores[0] - scores[1] === goalDiff || scores[1] - scores[0] === goalDiff){
+                            // TODO: Set condition to satisfied and update db
+                            console.log("SATISFIED", condition)
+                            console.log("Time", Date.now());
+                            // TODO: call(mobile phone)
+                        }
+                        else {
+                            console.log("Goal condition not met");
+                        }
+                    }
+                    else {
+                        console.log("Time condition not met");
+                    }
+                })
+            }
 
-    var currGame = funcs.getFuture(gameArr[i]);
+            // gameSchema.findOne({game_id: gameId})
+            //     .then((game) => {
+            //         if (!game) {
+            //             console.log("Game not found...");
+            //             return
+            //         }
+            //         else {
+            //             console.log("Found", game.game_id);
+            //         }
+            //
+            //         var prevState = game
+            //     })
+        })
+    })
+}, 5 * 60 * 1000)
 
+function addCondition(type, game_id, team, time, goals){
+    var condition = new conditionSchema({
+        type,
+        satisfied: false,
+        game_id,
+        team,
+        time,
+        goals
+    })
+    condition.save();
 }
 
-// route to scrape and save future games
-// app.get('/saveGames', (req,res) => {
-
-// });
+// addCondition(1, 508493, 0, 10, 1)
+// for(var i = 0; i < gameArr.length; i++){
+//     var currGame = funcs.getFuture(gameArr[i]);
+// }
--- models/conditions.js
+++ models/conditions.js
@@ -2,9 +2,26 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var conditionSchema = new Schema({
+	type: {type: Number},
+	satisfied: {type: Boolean},
 	game_id: {type: String},
-	team: {type: Number},
-	goals: {type: Number}
+	time: {type: Number},
+	goals: {type: Number},
+	team: {
+		type: Number,
+		required: function(){
+			return this.type === 2
+		}
+	}
 });
 
 mongoose.model('conditionSchema', conditionSchema);
+
+/*
+Conditions:
+
+1. Goal difference + At Time
+2. Goals Scored + For Team [At Time]
+3. Team X is Winning/Losing at Time
+
+*/
